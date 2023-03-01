afb9a47ce1e6544883ef828a192792709b3856ee
youngkyu.chung <n01468828@humbermail.ca>
2023-03-01 14:44:02 -0500

    change the each fragment page color and make a titles

--- app/src/main/java/youngkyu/chung/Lab5/ChungActivity.java
+++ app/src/main/java/youngkyu/chung/Lab5/ChungActivity.java
@@ -1,14 +1,56 @@
 package youngkyu.chung.Lab5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
-public class ChungActivity extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+
+public class ChungActivity extends AppCompatActivity implements TabLayoutMediator.TabConfigurationStrategy {
+    // implements is extend interface
+    private ViewPager2 viewPager2;
+    private TabLayout tabLayout;
+    ArrayList<String> titles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager2 = findViewById(R.id.viewPager2);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        setViewPagerAdapter();
+
+        titles = new ArrayList<String>();
+        titles.add("Home");
+        titles.add("Settings");
+        titles.add("Favorites");
+
+        new TabLayoutMediator(tabLayout, viewPager2, this).attach();
+    }
+    public void setViewPagerAdapter() {
+        ViewPager2Adapter viewPager2Adapter = new ViewPager2Adapter(this);
+
+        ArrayList<Fragment> fragmentList = new ArrayList<>(); //creates an ArrayList of Fragments
+
+        fragmentList.add(new HomeFragment());
+        fragmentList.add(new SettingsFragment());
+        fragmentList.add(new FavoritesFragment());
+
+        viewPager2Adapter.setData(fragmentList);
+        viewPager2.setAdapter(viewPager2Adapter);
+
+    }
+
+    @Override
+    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+
     }
 }
